c6f659ee021d16de88627ba4259ea813cc503f4d
Navdeep Sharma <navdeepsharmatu@gmail.com>
2023-12-21 20:28:07 +0530

    added postConstruct annotation code

---
+++ example6/src/main/java/com/example/main/Example6.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example6 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle veh = context.getBean(Vehicle.class);
+        // will print Honda bcoz of PostConstruct
+        System.out.println("Component Vehicle name from Spring Context is: " + veh.getName());
+        veh.printHello();
+    }
+}
