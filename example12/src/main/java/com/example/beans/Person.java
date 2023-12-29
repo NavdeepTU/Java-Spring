d64eae369acd0b7085d63221c280877c60efc374
Navdeep Sharma <navdeepsharmatu@gmail.com>
2023-12-29 18:33:15 +0530

    autowiring using bean name, primary and qualifier anotation

--- example12/src/main/java/com/example/beans/Person.java
+++ example12/src/main/java/com/example/beans/Person.java
@@ -1,6 +1,7 @@
 package com.example.beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,9 +11,9 @@ public class Person {
     private final Vehicle vehicle;
 
     @Autowired
-    public Person(Vehicle vehicle1) {
+    public Person(@Qualifier("vehicle2") Vehicle vehicle) {
         System.out.println("Person bean created by Spring");
-        this.vehicle = vehicle1;
+        this.vehicle = vehicle;
     }
 
     public String getName() {
--- example12/src/main/java/com/example/config/ProjectConfig.java
+++ example12/src/main/java/com/example/config/ProjectConfig.java
@@ -11,6 +11,10 @@ Spring @Configuration annotation is part of the spring core framework.
 Spring Configuration annotation indicates that the class has @Bean definition
 methods. So Spring container can process the class and generate Spring Beans
 to be used in the application.
+
+To tell Spring it needs to search for classes annotated
+with stereotype annotations, we use the @ComponentScan annotation over the
+configuration class.
 * */
 @Configuration
 @ComponentScan(basePackages = "com.example.beans")
@@ -31,6 +35,7 @@ public class ProjectConfig {
     }
 
     @Bean
+    // @Primary
     Vehicle vehicle3() {
         var veh = new Vehicle();
         veh.setName("Ferrari");
