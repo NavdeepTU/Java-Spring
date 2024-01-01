b203fca96c3a7c478152ead3a6c5a973ca562273
Navdeep Sharma <navdeepsharmatu@gmail.com>
2024-01-01 19:31:07 +0530

    added beans, interfaces and their implemention code

---
+++ example13/src/main/java/com/example/interfaces/Speakers.java
@@ -0,0 +1,11 @@
+package com.example.interfaces;
+
+public interface Speakers {
+
+    public String makeSound();
+}
+
+/**
+    cannot use annotation above interfaces as they cannot be beans.
+    beans can only be created from java classes.
+ */
